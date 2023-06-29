2fbb795a28964dcc597b21becbeaa58f78cb15de
Souvik00076 <souvikbhattacharjee00076@gmail.com>
2023-06-29 22:07:50 +0530

    chat screen and chat bubble added

---
+++ app/src/main/java/com/example/talkhive/fragments/ChatScreen.java
@@ -0,0 +1,14 @@
+package com.example.talkhive.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.talkhive.R;
+public class ChatScreen extends Fragment {
+
+}
